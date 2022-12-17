8b505310e40c02d1a7d2c807e6c296d056e7090f
JustiniLinguini <justinusprime@gmail.com>
2022-12-17 06:03:55 -0500

    add card styling to main content

--- src/home.js
+++ src/home.js
@@ -12,6 +12,7 @@ function makeDiv(){
 }
 
 export default function homeCardComponent(){
+    //heading container
     const headingContainer = makeDiv('heading-container');
     const heading = document.createElement('h1');
     const jellyEmoji = document.createElement('img');
@@ -21,8 +22,27 @@ export default function homeCardComponent(){
     headingContainer.appendChild(heading);
     headingContainer.appendChild(jellyEmoji);
 
+    //card quote
+
+    const quote = document.createElement('p');
+    quote.textContent = `We love Dancing Jelly's in our family,
+    it is the finest Jelly establishment in all of the coast!
+    I highly recommend trying a jelly margherita, it'll blow
+    your tentacles off!`;
+    quote.classList.add('quote')
+
+    const author = document.createElement('p');
+    author.textContent = "- Squidward Tentacles";
+    author.classList.add('author');
+
+    const quoteCard = makeDiv('card');
+    quoteCard.appendChild(quote);
+    quoteCard.appendChild(author);
+
+
     const mainCard = makeDiv('main-card');
     mainCard.appendChild(headingContainer);
+    mainCard.appendChild(quoteCard);
 
     return mainCard;
 }
